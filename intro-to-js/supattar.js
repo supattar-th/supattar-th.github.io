117d90804fc1aac02b189c5d1f6132c1aa070a93
supattar thueanthanon <supattar3522@gmail.com>
2022-02-20 21:06:11 +0700

    .

--- intro-to-js/supattar.js
+++ intro-to-js/supattar.js
@@ -11,3 +11,19 @@ function askMyName() {
     alert(h1message + ' "รถยังมีอะไหล่ แต่หัวใจไม่มีสำรอง"')
  }
 
+ function calAge() {
+     var inpytAge = prompt("Please enter your age", "Enter your name here.");
+        document.getElementById("calAge").innerHTML = "My age is " + age;
+        alert("ยินดีด้วย หร่อนอยู่ในประเทศนี้มา" + (2022 - inputAge) + "ปีแล้ว")
+        
+     }
+ }
+
+ function loadProfile() {
+     var name = "Your name"
+    var province = "Your province"
+     document.getElementById("my-name").innerHTML = name;
+     document.getElementById("my-province").innerHTML = province;
+ }
+
+ 
